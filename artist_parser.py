47ebe2c87b5452ff52934273fa5912edc46686e1
Ryan Maciel <rspams1234@gmail.com>
2020-05-10 13:40:55 -0400

    Started work organizing artists around movements

--- artist_parser.py
+++ artist_parser.py
@@ -10,6 +10,17 @@ painters_path = data_path + 'painters/'
 painters_list_path = painters_path + 'lists/'
 painters_names_path = painters_path + 'names/'
 
+# given a percentage, print a progress bar
+def print_progress(percentage):
+    num_slices = 20
+
+    num_done = int(percentage * num_slices)
+    right_pad = num_slices - num_done
+
+    print('[' + ('#' * num_done) + (' ' * right_pad) + '] ' + str(int(percentage * 100)) + '%', end='\r')
+
+    if int(percentage) == 1:
+        print('\n')
 
 def parse_artist_page(html_string):
 
@@ -25,8 +36,37 @@ def parse_artist_page(html_string):
             key_html = tr.find('th')
             value_html = tr.find('td')
 
-            if key_html and value_html:     
-                attributes[key_html.text.replace('\xa0', ' ')] = value_html.text.replace('\xa0', ' ')
+            if key_html and value_html:
+
+                # parse the movements into a list
+                # this is difficult because sometimes movements are linked,
+                # and sometimes they are just listed (with different formats lol)
+                if key_html.text == 'Movement':
+
+                    movement_list = []
+
+                    # find all linked movements
+                    # TODO: sometimes links to the same movement have different labels.
+                    # This could be a relatively easy fix.
+                    movement_links = value_html.find_all('a')
+                    for movement in movement_links:
+                        if movement.parent.name != 'sup':
+                            movement_list.append(movement.text.replace('\xa0', ' '))
+                    
+                    # try to make sense of the raw text
+                    unlinked_results = value_html.find_all(text=True, recursive=False)
+                    for result in unlinked_results:
+
+                        # don't want one of these common separation words.
+                        if result.strip() not in ['/', 'to', 'and', ',']:
+                            sub_movements = result.strip().replace('\xa0', ' ').split(',')
+                            for sub in sub_movements:
+                                movement_list.append(sub)
+                    attributes[key_html.text.replace('\xa0', ' ')] = movement_list
+                else:
+                    # TODO deal with unicode issues. You might have to deal with langauge issues which will
+                    # take you all the way back to the downloader which might not be fun...   
+                    attributes[key_html.text.replace('\xa0', ' ')] = value_html.text.replace('\xa0', ' ')
         
         return attributes
 
@@ -48,16 +88,35 @@ def main(argv):
 
     infobox_results = {}
 
-    for i in range(len(artist_pages)):
-        name = artist_pages[i].replace('.html', '')
+    # for i in range(len(artist_pages)):
+    #     name = artist_pages[i].replace('.html', '')
+    #     html_string = open(painters_names_path + artist_pages[i], 'r').read()
+    #     infobox_results[name] = parse_artist_page(html_string)
 
-        html_string = open(painters_names_path + artist_pages[i], 'r').read()
-        res = parse_artist_page(html_string)
-        infobox_results[name] = res
+    #     if i % 100 == 0:
+    #         with open('data.json', 'w') as outfile:
+    #             json.dump(infobox_results, outfile)
+        
+    #     print_progress(i/len(artist_pages))
 
-        if i % 100 == 0:
-            with open('data.json', 'w') as outfile:
-                json.dump(infobox_results, outfile)
+    data_string = open('data.json', 'r').read()
+    data = json.loads(data_string)
+    
+    movements = {}
+    for artist_name in data:
+        item = data[artist_name]
+        if item != None and 'Movement' in item:
+            movement_list = item['Movement']
+            for movement in movement_list:
+                if movement not in movements:
+                    movements[movement] = 1
+                else: 
+                    movements[movement] += 1
+    
+    from collections import OrderedDict
+    sort_movements = OrderedDict(sorted(movements.items(), key=lambda a: a[1], reverse=True))
+    pp.pprint(sort_movements)
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
